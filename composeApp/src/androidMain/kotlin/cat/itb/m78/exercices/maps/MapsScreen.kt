c11449e3462f3a0812093adccffc194d7ff3c621
LauraZaporta <laura.zaporta.7e8@itb.cat>
2025-04-24 10:03:46 +0200

    Base de dades creada

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/maps/MapsScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/maps/MapsScreen.kt
@@ -1,5 +1,6 @@
 package cat.itb.m78.exercices.maps
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,26 +10,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.AdvancedMarker
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun MapsScreen(){
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 11f)
-    }
-    Box(Modifier.fillMaxSize()) {
-        GoogleMap(cameraPositionState = cameraPositionState)
-        Button(onClick = {
-            // Move the camera to a new zoom level
-            cameraPositionState.move(CameraUpdateFactory.zoomIn())
+    GoogleMap(
+        googleMapOptionsFactory = {
+            GoogleMapOptions().mapId("DEMO_MAP_ID")
         },
-            modifier = Modifier.padding(top = 50.dp, start = 10.dp)) {
-            Text(text = "Zoom In")
-        }
+    ) {
+        AdvancedMarker(
+            state = MarkerState(position = LatLng(-34.0, 151.0)),
+            title = "Marker in Sydney"
+        )
+        AdvancedMarker(
+            state = MarkerState(position = LatLng(35.66, 139.6)),
+            title = "Marker in Tokyo"
+        )
     }
 }
